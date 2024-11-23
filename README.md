39b398577d4a4f93b4e46dba820d34a81b626c52
Yash Malik Lenovo Portable <yash.malik0606@gmail.com>
2024-11-23 15:51:00 -0600

    Added README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hello!
+
+This repo is for my exploration of ruby to add a programming language to my skillset.
+
--- helloworld.rb
+++ helloworld.rb
@@ -1,9 +1,18 @@
 puts "Hello World"
-
-= begin
+# Comment?
+=begin
 This is a multiline comment
 
 Now it ends
-= end
+=end
 
 puts "John has #{25+30/6} Dollars in his pocket"
+
+students = 4
+classes = 6
+
+puts "There are #{students} students and #{classes} classes."
+
+print "Think of a number: "
+num = gets.chomp
+puts "You thought of #{num}. I thought of #{num.to_i + 1}. I win!"
